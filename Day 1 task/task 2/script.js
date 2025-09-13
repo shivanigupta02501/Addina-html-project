0f21ccfa985ba821af7a4c0b92aa937841f9814c
Shivani Gupta <shivanigupta02501@gmail.com>
2025-09-13 17:47:23 +0530

    changes

---
+++ Day 1 task/task 2/script.js
@@ -0,0 +1,13 @@
+let a=Number(prompt("enter the value of a :"));
+let b=Number(prompt("enter the value of b :"));
+
+console.log(`Addition of a & b : ${a+b}`);
+console.log(`Substraction of a & b : ${a-b}`);
+console.log(`Multiplication of a & b : ${a*b}`);
+console.log(`Division of a & b : ${a/b}`);
+
+document.write(`<br>value of a:${a} & value of b:${b}`)
+document.write(`<br>Addition of a & b : ${a+b}`);
+document.write(`<br>Substraction of a & b : ${a-b}`);
+document.write(`<br>Multiplication of a & b : ${a*b}`);
+document.write(`<br>Division of a & b : ${a/b}`);
